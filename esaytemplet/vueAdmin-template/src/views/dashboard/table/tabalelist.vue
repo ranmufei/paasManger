68f0726edc97fdd26a9545c673da220d84aa9b20
冉慕飞 <362134111@qq.com>
2018-03-27 14:26:30 +0800

    paas 项目

---
+++ esaytemplet/vueAdmin-template/src/views/dashboard/table/tabalelist.vue
@@ -0,0 +1,65 @@
+ <template>
+ <div>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+
+    <el-button type="info" @click="getdata">信息按钮</el-button>
+</div>
+  </template>
+
+  <script>
+  import { fetchList } from '@/api/getTableData'
+
+    export default {
+      data() {
+        return {
+          tableData: [{
+            date: '2016-05-02',
+            name: '王小虎22',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+        }
+      },
+      methods:{
+        getdata(){
+         
+          fetchList().then(response => {
+            //this.tableData = response.data.items.slice(0, 7)
+              console.log('表格远程获取的数据：',response.data)
+             this.tableData = response.data
+          },function(err){
+              console.log('表格远程获取的数据err：',err)
+          })
+          
+        }
+      }
+    }
+  </script>
---
+++ esaytemplet/vueAdmin-template/src/views/layout/components/Sidebar/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <scroll-bar>
+    <h4 class="avator"><img src="src/assets/avator.jpg"></h4>
+    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
+      <sidebar-item :routes="routes"></sidebar-item>
+    </el-menu>
+  </scroll-bar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
+import ScrollBar from '@/components/ScrollBar'
+
+export default {
+  components: { SidebarItem, ScrollBar },
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    routes() {
+      return this.$router.options.routes
+    },
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .avator{
+    width:80px;
+    height: 80px;
+    margin: 40px auto;
+    z-index: -1;
+  }
+  .avator img{
+    border-radius:50%;
+    width:80px;
+    height: 80px;
+  }
+</style>
